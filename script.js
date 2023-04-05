431a6c9b2e3ea19f602db1b6eea946ad1da5abb4
luigipr <luigiprofeti@gmail.com>
2023-04-04 22:17:20 -0300

    versão com o gerador de cartas

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-const cards = Number(prompt('Com quantas cartas você quer jogar? 4-14'))
+const arrayCartas = [];
+let numberOfCards = '';
+function cardnumber() {
+    numberOfCards = Number(prompt('Com quantas cartas você quer jogar? 4-14'));
+}
+cardnumber();
+const container = document.querySelector('.container');
+// Create the specified number of divs using a loop
+for (let i = 0; i < numberOfCards; i++) {
+    if (3 < numberOfCards && numberOfCards < 15 && numberOfCards % 2 === 0) {   
+         let div = document.createElement("div");
+        div.classList.add('card');
+        console.log(div);
+        front = document.createElement('div')
+        console.log(front.parentNode)
+        div.innerHTML = "<img src='midia/back.png'>"
+        container.appendChild(div);
+} else {
+    i -= 1;
+    cardnumber();
+}}
+
+
+
+
