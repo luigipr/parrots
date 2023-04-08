60e0b5b3ece3aea45b0e9fc64aa449ceeb70750a
luigipr <luigiprofeti@gmail.com>
2023-04-08 14:50:54 -0300

    versão com as cartas virando corretamente e identificando os pares

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const arrayCartas = [];
 let arrayback = [];
-let numberOfCards = '';
-const arraybackface = ["<img src='midia/bobrossparrot.gif'>", "<img src='midia/bobrossparrot.gif'>", "<img src='midia/explodyparrot.gif' />", "<img src='midia/explodyparrot.gif' />",
-"<img src='midia/fiestaparrot.gif' />", "<img src='midia/fiestaparrot.gif' />", "<img src='midia/metalparrot.gif' />", "<img src='midia/metalparrot.gif' />", "<img src='midia/revertitparrot.gif' />",
-"<img src='midia/revertitparrot.gif' />", "<img src='midia/tripletsparrot.gif' />", "<img src='midia/tripletsparrot.gif' />", "<img src='midia/unicornparrot.gif' />",
- "<img src='midia/unicornparrot.gif' />"];
+let previousCard = null;
+let turnedCards = [];
+let numCards = 0;
+let rightCards  = 0;
+let numMoves = 0;
+let interval = false;
+let card1, card2;
+let hasFlippedCard = false;
+const arraybackface = ['midia/bobrossparrot.gif','midia/bobrossparrot.gif','midia/explodyparrot.gif','midia/explodyparrot.gif','midia/fiestaparrot.gif','midia/fiestaparrot.gif',
+'midia/metalparrot.gif','midia/metalparrot.gif','midia/revertitparrot.gif','midia/revertitparrot.gif','midia/tripletsparrot.gif','midia/tripletsparrot.gif','midia/unicornparrot.gif',
+ 'midia/unicornparrot.gif' ];
 
 function cardnumber() {
     numberOfCards = Number(prompt('Com quantas cartas você quer jogar? 4-14'));
@@ -36,20 +42,23 @@ const container = document.querySelector('.container');
 // Create the specified number of divs using a loop
 for (let i = 0; i < numberOfCards; i++) {
     if (3 < numberOfCards && numberOfCards < 15 && numberOfCards % 2 === 0) {   
-        let div = document.createElement("div");
+      let div = document.createElement("div");
         div.classList.add('card');
-        let front = document.createElement('div');
-        let back = document.createElement('div');
+        
+        let front = document.createElement('img');
+        let back = document.createElement('img');
         //front.classList.add('front')
         front.classList.add('face');
         front.classList.add('front-face');
-        back.innerHTML = arrayback[i];
+        back.setAttribute("src" , 'midia/back.png');
         back.classList.add('back-face');
         back.classList.add('face');
         //back.classList.add('back');
-        front.innerHTML = "<img src='midia/back.png'>";
-        div.setAttribute('onclick', 'flipcard(this)')
+        front.setAttribute("src" , arrayback[i]);
+        front.innerHTML = arrayback[i];
+        div.setAttribute('onclick', 'flipCard(this)');
         console.log(div) ;  
+        div.setAttribute('data-framework', arrayback[i]);
         container.appendChild(div);
         div.appendChild(front);
         div.appendChild(back);
@@ -67,18 +76,105 @@ for (let i = 0; i < numberOfCards; i++) {
 } 
 }
 
-function flipcard(card) {
-    console.log(card)
 
-    const front = card.querySelector('.front-face');
-    console.log(front)
-    front.classList.toggle('front');
+function flipCard(card) {
+    numMoves++;
+    card.classList.add('flip');
 
-    const back = card.querySelector('.back-face');
-    console.log(back)
-    back.classList.toggle('back');
+    if(!hasFlippedCard) {
+      hasFlippedCard = true;
+      card1 = card;
+    } else {
+      hasFlippedCard = false;
+      card2 = card;
+    
+    console.log(card1, card2)
+    
+    if (card1.dataset.framework === card2.dataset.framework) {
+      card1.removeAttribute('onclick');
+      card2.removeAttribute('onclick');
+    } else {
+      setTimeout(() => {
+        card1.classList.remove('flip');
+        card2.classList.remove('flip');
+      }, 2000 );
+    }
+    }
+
+//     card.classList.add('selected')
+//     const front = card.querySelector('.front-face');
+//     console.log(front)
+//     front.classList.add('front');
+
+//     const back = card.querySelector('.back-face');
+//     console.log(back)
+//     back.classList.add('back');
+//     card.removeAttribute("onclick")
+// //    verifyPair();
 
 }
+
+// function verifyPair() {
+//     turnedCards = document.querySelectorAll('.selected');
+//     if (turnedCards.length === 2){
+//         card1 = turnedCards[0];
+//         card2 = turnedCards[1];
+//         if (card1.id === card2.id) {
+//             card1.classList.remove('selected');
+//             card2.classList.remove('selected');
+//             card1.classList.add('par');
+//             card2.classList.add('par');
+//         }
+
+
+//     }
+    
+
+//     rightCards += 2;
+
+//     if (rightCards === numCards) {
+//       endGame();
+//     }
+//   } else{
+//     setTimeout ( returnCards , 2000 ); 
+//   }
+// } 
+// }
+
+function returnCards(){
+for (let i=0; i < turnedCards.length; i++){
+  if ( turnedCards[i].classList.contains("review") ){
+    turnedCards[i].classList.remove("review");
+    turnedCards[i].classList.remove("front");
+    turnedCards[i].classList.add ("back");
+
+    turnedCards[i].setAttribute("data-identifier","back-face");
+  }
+}
+}
+
+
+
+
+
+
+function endGame(){
+clearInterval(interval);
+alert (`Você ganhou em ${numMoves} jogadas e em ${counter.innerHTML} segundos!`);
+
+let restart = prompt("Deseja reiniciar a partida? (Sim ou Não)");
+
+if (restart.toUpperCase() === "SIM" || restart.toUpperCase() === "S"){
+  restartGame();
+}
+}
+
+
+
+
+
+
+
 
 
 // const arrayCardsFront = document.querySelectorAll('.front-face')
