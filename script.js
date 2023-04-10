fff8f51a489b0d5a98b5c85b856888a2ffdfa8f3
luigipr <luigiprofeti@gmail.com>
2023-04-10 16:35:00 -0300

    versão completa porém sem impedir o terceiro clique

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let interval = 0;
 let count = 0;
 let card1, card2;
 let hasFlippedCard = false;
+let flippedcards = 0;
 const arraybackface = ['midia/bobrossparrot.gif','midia/bobrossparrot.gif','midia/explodyparrot.gif','midia/explodyparrot.gif','midia/fiestaparrot.gif','midia/fiestaparrot.gif',
 'midia/metalparrot.gif','midia/metalparrot.gif','midia/revertitparrot.gif','midia/revertitparrot.gif','midia/tripletsparrot.gif','midia/tripletsparrot.gif','midia/unicornparrot.gif',
  'midia/unicornparrot.gif' ];
@@ -39,6 +40,7 @@ randomizador();
 
 const container = document.querySelector('.container');
 // Create the specified number of divs using a loop
+function cardCreator(numberOfCards) {
 for (let i = 0; i < numberOfCards; i++) {
     if (3 < numberOfCards && numberOfCards < 15 && numberOfCards % 2 === 0) {   
       let div = document.createElement("div");
@@ -76,55 +78,78 @@ for (let i = 0; i < numberOfCards; i++) {
         // </div>    
         // console.log(div);
         // console.log(front.parentNode);
-        arrayCartas.push(back);
+        arrayCartas.push(div);
         // console.log(arrayCartas);
-           
+        console.log(arrayCartas)
 } 
 }
-
+}
+cardCreator(numberOfCards);
 
 function flipCard(card) {
     numMoves++;
+    if (flippedcards < 2) {
     card.classList.add('flip');
+    } 
+    if (flippedcards === 2) {
+      const vira = document.querySelectorAll('.flip')
+      console.log(vira)
+    }
+    
 
     if(!hasFlippedCard) {
       hasFlippedCard = true;
       card1 = card;
+      flippedcards++;
     } else {
       hasFlippedCard = false;
       card2 = card;
-    
-    console.log(card1, card2)
+      flippedcards++;
+     console.log(card1, card2);
 
       }
-    const flippedcards = document.querySelectorAll('.flip');
-      if ((flippedcards.length)%2 === 0) {
+      check();
+    }
+    
+    function check() {
+      if (flippedcards%2 === 0) {
         if (card1.dataset.framework === card2.dataset.framework) {
           card1.removeAttribute('onclick');
           card2.removeAttribute('onclick');
-          rightCards += 2
+          rightCards += 2;
+          flippedcards = 0;
         } else {      
           
           setTimeout(() => {
             card1.classList.remove('flip');
             card2.classList.remove('flip');
+            flippedcards = 0;
           }, 1000 );
         }
       }  
-    }
-
+    
     setTimeout(() => {
       if (rightCards === numberOfCards) {
       endGame();
-    }}, 1500 );
+    }}, 1000 );
+  }
     
+  // function restartGame () {
+  //   const oldcards = document.querySelectorAll('.cards');
+  //   oldcards.forEach(card => remove(card));
+  //   const ncards = cardnumber();
+  //   backCards(ncards);
+  //   randomizador();
+  //   cardCreator(ncards);
+  //   rightCards = 0;
+  // }
 
     function endGame(){
       clearInterval(interval);
       alert (`Você ganhou em ${numMoves} jogadas!`);
-      
-      //let restart = prompt("Deseja reiniciar a partida? (Sim ou Não)");
-      
+
+
+      // let restart = prompt("Deseja reiniciar a partida? (Sim ou Não)");      
       // if (restart.toUpperCase() === "SIM" || restart.toUpperCase() === "S"){
       //   restartGame();
       // }
